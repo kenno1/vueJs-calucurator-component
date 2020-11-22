1fa3c4d22e74c7612a57e67ae334feadfab2d8b3
kambe.kenta <kenta.k199405@gmail.com>
2020-11-22 14:58:43 +0900

    initialize repository.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calucurator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calucurator from './components/Calucurator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calucurator
   }
 }
 </script>
